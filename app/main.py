0d5120a595801dae56bced6243ea619ed515502f
akshaymitra-tredence <akshay.mitra@tredence.com>
2025-08-02 14:24:57 +0530

    made a change in main.py as per request

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def greet(name: str) -> str:
     return f"Hello, {name}! Welcome to secure Git workflows."
-print("Feature X enabled!")
+print("Feature Z enabled!")
 
 if __name__ == "__main__":
     print(greet("Akshay"))
