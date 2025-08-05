364d4bbee499568a8bd03ca1e948f067235d59e9
akshaymitra-tredence <akshay.mitra@tredence.com>
2025-08-05 14:19:31 +0530

    added codeql syntax

--- app/main.py
+++ app/main.py
@@ -1,14 +1,11 @@
-import os
+import sys
 
 def run_user_code(user_input):
     eval(user_input)
 
 if __name__ == "__main__":
-    file_path = "untrusted_input.txt"
-    if os.path.exists(file_path):
-        with open(file_path, "r") as f:
-            user_input = f.read()
-        print("Running code from file...")
+    if len(sys.argv) > 1:
+        user_input = sys.argv[1]
         run_user_code(user_input)
     else:
-        print("untrusted_input.txt not found. Exiting.")
+        print("Please provide a command-line argument.")
